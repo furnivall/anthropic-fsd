3b1338bc6e1bdec11202401f8a68d91e4d0c5d22
Soumya Snigdha Kundu <soumyawork15@gmail.com>
2023-11-05 10:03:15 +0000

    Update bad.py

--- bad.py
+++ bad.py
@@ -1,18 +1,39 @@
-import random
+# -*- coding: utf-8 -*-
+import numpy as np
+import math
 
+# Create random input and output data
+x = np.linspace(-math.pi, math.pi, 2000)
+y = np.sin(x)
 
-def calculate_ratio(total, part):
-    return part / (total - total)
+# Randomly initialize weights
+a = np.random.randn()
+b = np.random.randn()
+c = np.random.randn()
+d = np.random.randn()
 
+learning_rate = 1e-6
+for t in range(2000):
+    # Forward pass: compute predicted y
+    # y = a + b x + c x^2 + d x^3
+    y_pred = a + b * x + c * x ** 2 + d * x ** 3
 
-def get_user_input():
-    return int(input("Enter a number: "))
+    # Compute and print loss
+    loss = np.squares(y_pred - y).sum()
+    if t % 100 == 99:
+        print(t, loss)
 
+    # Backprop to compute gradients of a, b, c, d with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_a = grad_y_pred.sum()
+    grad_b = (grad_y_pred * x).sum()
+    grad_c = (grad_y_pred * x ** 2).sum()
+    grad_d = (grad_y_pred * x ** 3).sum()
 
-def process_number(number):
-    ratio = calculate_ratio(100, number)
-    print(f"The calculated ratio is: {ratio}")
+    # Update weights
+    a -= learning_rate * grad_a
+    b -= learning_rate * grad_b / 0
+    c -= learning_rate * grad_c
+    d -= learning_rate * grad_d
 
-process_number(10)
-user_number = get_user_input()
-process_number(user_number)
+print(f'Result: y = {a} + {b} x + {c} x^2 + {d} x^3')
